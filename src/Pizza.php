e5c67d5eada7c6bce1554283d6427fa4cfa5d6a9
abella <annette.2003@mail.ru>
2023-11-02 09:14:59 +0300

    fix var

--- src/Pizza.php
+++ src/Pizza.php
@@ -5,13 +5,13 @@ namespace Abellart\Pizzastore;
 abstract class Pizza
 {
     public string $name;
-    public string $sause;
+    public string $sauce;
     public array $toppings;
 
     public function prepare(): void
     {
         echo "Началась готовка пиццы " . $this->name . PHP_EOL;
-        echo "Добавлен соус " . $this->sause . PHP_EOL;
+        echo "Добавлен соус " . $this->sauce . PHP_EOL;
         echo "Добавлены топики " . implode($this->toppings) . PHP_EOL;
     }
 
